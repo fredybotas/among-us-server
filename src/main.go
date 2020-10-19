7ee0e05fe5034eaacb537a7d215f7fba456e1b09
Michal Manak <fredybotas@gmail.com>
2020-10-19 19:27:07 +0200

    Server listen added

--- src/main.go
+++ src/main.go
@@ -1,39 +1,10 @@
 package main
 
 import (
-	"fmt"
-	"net"
+	"server"
 )
 
-func sendResponse(conn *net.UDPConn, addr *net.UDPAddr) {
-	_, err := conn.WriteToUDP([]byte("From server: Hello I got your message "), addr)
-
-	if err != nil {
-		fmt.Printf("Couldn't send response %v", err)
-	}
-}
-
 func main() {
-	fmt.Printf("Starting server\n")
-	p := make([]byte, 1024)
-	addr := net.UDPAddr{
-		Port: 1234,
-		IP:   net.ParseIP("127.0.0.1"),
-	}
-	server, err := net.ListenUDP("udp", &addr)
-	if err != nil {
-		fmt.Printf("Some error %v\n", err)
-		return
-	}
-
-	for {
-		_, remoteaddr, err := server.ReadFromUDP(p)
-		fmt.Printf("Read a message from %v %s ,,,\n", remoteaddr, p)
-		fmt.Printf("Length %v\n", len(p))
-		if err != nil {
-			fmt.Printf("Some error  %v", err)
-			continue
-		}
-		go sendResponse(server, remoteaddr)
-	}
+	var serv server.UDPServer
+	serv.Init()
 }
--- src/server/server.go
+++ src/server/server.go
@@ -1,3 +1,27 @@
 package server
 
-const Port string = ":1221"
+import (
+	"fmt"
+	"net"
+)
+
+const port int = 1221
+const ip string = "127.0.0.1"
+
+type UDPServer struct {
+	socket *net.UDPConn
+}
+
+func (server *UDPServer) Init() {
+	fmt.Printf("Starting server\n")
+	addr := net.UDPAddr{
+		Port: port,
+		IP:   net.ParseIP(ip),
+	}
+	sock, err := net.ListenUDP("udp", &addr)
+	if err != nil {
+		fmt.Printf("Error binding sock %v\n", err)
+		return
+	}
+	server.socket = sock
+}
