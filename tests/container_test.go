a08be0191ce7492cc5ad8e776c6595d26cefc8f9
Michal Manak <fredybotas@gmail.com>
2020-10-20 16:16:20 +0200

    Add container tests

---
+++ tests/container_test.go
@@ -0,0 +1,98 @@
+package tests
+
+import (
+	"cont"
+	"math/rand"
+	"testing"
+	"time"
+)
+
+func initContainer() *cont.Container {
+	var container cont.Container
+	container.Init()
+	return &container
+}
+
+var seededRand *rand.Rand = rand.New(rand.NewSource(time.Now().UnixNano()))
+
+func StringWithCharset(length int, charset string) string {
+	b := make([]byte, length)
+	for i := range b {
+		b[i] = charset[seededRand.Intn(len(charset))]
+	}
+	return string(b)
+}
+
+func TestContainerAdd(t *testing.T) {
+	container := initContainer()
+	inserted := container.InsertEntry(cont.NewEntry("aaa", 1, 2))
+	if inserted == false {
+		t.Errorf("Element was not inserted")
+	}
+}
+
+func TestContainerAddAndFetch(t *testing.T) {
+	container := initContainer()
+	container.InsertEntry(cont.NewEntry("aaa", 1, 2))
+	res := container.Query(cont.NewLocation(1, 2), 1)
+	if len(res) != 1 {
+		t.Errorf("Element was not inserted")
+	}
+
+	if res[0].GetCode() != "aaa" {
+		t.Errorf("Received wrong element")
+	}
+}
+
+func TestContainerAddDuplicate(t *testing.T) {
+	container := initContainer()
+	container.InsertEntry(cont.NewEntry("aaa", 1, 2))
+	inserted := container.InsertEntry(cont.NewEntry("aaa", 1, 2))
+	if inserted == true {
+		t.Errorf("Element inserted twice")
+	}
+	res := container.Query(cont.NewLocation(1, 2), 1)
+	if len(res) != 1 {
+		t.Errorf("Element was not inserted")
+	}
+
+	if res[0].GetCode() != "aaa" {
+		t.Errorf("Received wrong element")
+	}
+}
+
+func TestContainerAddConcurrently(t *testing.T) {
+	container := initContainer()
+
+	for i := 0; i < 1000; i++ {
+		go container.InsertEntry(cont.NewEntry(StringWithCharset(3, "abcdefghijklmnop"), 1, 2))
+	}
+
+	if container.GetCount() == 0 {
+		t.Errorf("Elements were not inserted")
+	}
+}
+
+func TestContainerAddAndReadConcurrently(t *testing.T) {
+	container := initContainer()
+
+	for i := 0; i < 100000; i++ {
+		go container.InsertEntry(cont.NewEntry(StringWithCharset(5, "abcdefghijklmnop"), 1, 2))
+		go container.Query(cont.NewLocation(1, 2), 1)
+	}
+
+	if container.GetCount() == 0 {
+		t.Errorf("Elements were not inserted")
+	}
+}
+
+func TestRemoveInactiveEntries(t *testing.T) {
+	container := initContainer()
+	for i := 0; i < 1000; i++ {
+		go container.InsertEntry(cont.NewEntry(StringWithCharset(5, "abcdefghijklmnop"), 1, 2))
+	}
+	time.Sleep((cont.CheckInterval*2 + 1) * time.Second)
+	if container.GetCount() != 0 {
+		t.Errorf("Elements were not deleted")
+	}
+}
